6b812f854d06e1c1a2ac33b08bd894f41eaa0bf9
jiang666 <934321800@qq.com>
2025-07-17 14:08:32 +0800

    清空

--- backend/src/main/java/com/example/course/repo/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.course.repo;
-
-import com.example.course.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CourseRepository extends JpaRepository<Course,String>,
-                                             JpaSpecificationExecutor<Course> { }
--- backend/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO roles(id,name) VALUES(UUID(), 'ROLE_ADMIN');
-INSERT INTO roles(id,name) VALUES(UUID(), 'ROLE_STUDENT');
-
-INSERT INTO users(id,username,password,enabled,created_at,updated_at) VALUES(UUID(),'admin','$2a$10$Dow1OX3N0NnfdXbra.mzeuFi1LXxax3CX6XcncxZV8oPZxC25f6/S',true,NOW(),NOW());
-
-INSERT INTO user_roles(user_id,role_id)
-SELECT u.id,r.id FROM users u, roles r WHERE u.username='admin' AND r.name='ROLE_ADMIN';
-
-INSERT INTO courses(id,title,cover,description,created_at,updated_at) VALUES(UUID(),'Sample Course','cover.jpg','demo description',NOW(),NOW());
