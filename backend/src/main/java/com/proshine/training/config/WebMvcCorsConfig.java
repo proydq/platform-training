f7a5f60ac9bd1ad37fca888abf1fe1059c82ad1b
jiang666 <934321800@qq.com>
2025-07-17 16:08:37 +0800

    修改简单错误

---
+++ backend/src/main/java/com/proshine/training/config/WebMvcCorsConfig.java
@@ -0,0 +1,16 @@
+package com.proshine.training.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
